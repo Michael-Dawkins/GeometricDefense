e9a4bf05d42a69f2a9d50d15f847ff119a52caa1
Michael Dawkins <michaeldawfr@gmail.com>
2014-08-17 16:07:01 +0200

    [Feat] Drag and drop to create towers

---
+++ Assets/Scripts/CreateTowerOnDrag.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateTowerOnDrag : MonoBehaviour {
+
+	public GameObject towerToCreate;
+	public GameObject lastTowerCreated;
+
+	private Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		Debug.Log ("Creating tower");
+		lastTowerCreated = Instantiate(towerToCreate, transform.position, transform.rotation) as GameObject;
+	}
+
+	void OnMouseDrag(){
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosition.z = 0;
+		lastTowerCreated.transform.position = mousePosition;
+		Debug.Log("Dragging tower at position " + lastTowerCreated.transform.position);
+	}
+}
