729b89d279e48438c32478e396fae8a9e830553a
Michael Dawkins <michaeldawfr@gmail.com>
2014-10-04 21:53:21 +0200

    [Feat] Tiles that light up upon enemy motion

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider2D))]
+public class Tile : MonoBehaviour {
+
+	public float timeToFadeOut;
+	SpriteRenderer spriteRenderer;
+	bool animationStarted = false;
+	Color targetColor = new Color(1f,1f,1f,0.2f);
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.color = targetColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animationStarted){
+			spriteRenderer.color = Color.Lerp(spriteRenderer.color, targetColor, Time.deltaTime * (1f / timeToFadeOut));
+			if (spriteRenderer.color.a == 0f){
+				animationStarted = false;
+			}
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		animationStarted = true;
+		spriteRenderer.color = Color.white;
+	}
+}
---
+++ Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileGenerator : MonoBehaviour {
+
+	public GameObject tilePrefab;
+
+	void Start () {
+		Map map = GameObject.Find("Map").GetComponent<Map>();
+		for( int x = 0; x < map.mapWidth; x++){
+			for(int y = 0; y < map.mapHeight; y++){
+				GameObject tile = Instantiate(tilePrefab) as GameObject;
+				tile.transform.position = new Vector3(x * map.cellSize + map.cellSize / 2f,y * map.cellSize + map.cellSize / 2f, 0);
+			}
+		}
+	}
+}
