05d449c2b2140cf39dbca6495786a8aa4d63ada2
Michael Dawkins <michaeldawfr@gmail.com>
2015-01-05 22:43:10 +0100

    [Feat] TimeScale selector for up to 3 times the normal gamespeed

---
+++ Assets/Scripts/Labels/TimeScaleLabel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TimeScaleLabel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TimeScaleManager.instance.TimeScaleChange += UpdateSpeedLabel;
+		UpdateSpeedLabel();
+	}
+
+	void UpdateSpeedLabel(){
+		GetComponent<Text>().text = TimeScaleManager.instance.GetCurrentTimeScaleLabel() + "X";
+	}
+
+}
---
+++ Assets/Scripts/Services/TimeScaleManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScaleManager : MonoBehaviour {
+
+	public static TimeScaleManager instance;
+	public delegate void OnTimeScaleChangeCallback();
+	public event OnTimeScaleChangeCallback TimeScaleChange;
+
+	public enum TimeScale{
+		ONE, TWO, THREE
+	}
+	public TimeScale timeScale;
+	void Awake(){
+		instance = this;
+		timeScale = TimeScale.ONE;
+	}
+	
+	public void SelectNextTimeScale(){
+		switch(timeScale){
+		case TimeScale.ONE:
+			timeScale = TimeScale.TWO;
+			Time.timeScale = 2f;
+			if (TimeScaleChange != null){
+				TimeScaleChange();
+			}
+			break;
+		case TimeScale.TWO:
+			timeScale = TimeScale.THREE;
+			Time.timeScale = 3f;
+			if (TimeScaleChange != null){
+				TimeScaleChange();
+			}
+			break;
+		case TimeScale.THREE:
+			timeScale = TimeScale.ONE;
+			Time.timeScale = 1f;
+			if (TimeScaleChange != null){
+				TimeScaleChange();
+			}
+			break;
+		}
+	}
+
+	public string GetCurrentTimeScaleLabel(){
+		switch(timeScale){
+			case TimeScale.ONE:
+				return "1";
+			case TimeScale.TWO:
+				return "2";
+			case TimeScale.THREE:
+				return "3";
+			default:
+				return "error";
+		}
+	}
+}
