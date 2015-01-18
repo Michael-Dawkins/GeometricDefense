2287d3a7567848a2bfa68b3d96afa48a6b145351
Michael Dawkins <michaeldawfr@gmail.com>
2015-01-18 21:36:06 +0100

    [Feat] FasterAtLowLife

---
+++ Assets/Scripts/Enemies/FasterAtLowLife.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CanMove))]
+[RequireComponent(typeof(CanTakeDamage))]
+public class FasterAtLowLife : MonoBehaviour {
+
+	float healthThreshold = 0.25f;//percentage expressed 0 to 1
+	CanMove canMoove;
+	CanTakeDamage canTakeDamage;
+
+	void Start () {
+		canMoove = GetComponent<CanMove>();
+		canTakeDamage = GetComponent<CanTakeDamage>();
+		canTakeDamage.healthChanged += GoFasterIfHealthIsLow;
+	}
+
+	void GoFasterIfHealthIsLow(){
+		float lifePercentage = canTakeDamage.currentHp / canTakeDamage.InitialHp;
+		canMoove.speedUp = (lifePercentage < healthThreshold);
+	}
+}
