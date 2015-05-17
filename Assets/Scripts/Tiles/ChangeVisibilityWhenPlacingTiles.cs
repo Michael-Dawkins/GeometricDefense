e7a64d9528442ed09d74177d721344e7089fc242
Michael Dawkins <michaeldawfr@gmail.com>
2015-05-17 19:22:40 +0200

    feat(tilePlacer): Change visibility when positioning tiles

---
+++ Assets/Scripts/Tiles/ChangeVisibilityWhenPlacingTiles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeVisibilityWhenPlacingTiles : MonoBehaviour {
+
+    private TilePlacer tilePlacer;
+
+    public bool VisibleWhenPlacingTiles;
+
+	void Start () {
+        tilePlacer = TilePlacer.instance;
+        tilePlacer.AddStartPositioningTilesListener(SetAppropriateVisibility);
+        tilePlacer.AddStopPositioningTilesListener(SetAppropriateVisibility);
+        SetAppropriateVisibility();
+	}
+
+    void SetAppropriateVisibility() {
+        if (VisibleWhenPlacingTiles == true) {
+            gameObject.SetActive(tilePlacer.IsPositioningTiles);
+        } else {
+            gameObject.SetActive(!tilePlacer.IsPositioningTiles);
+        }
+    }
+
+    void OnDestroy() {
+        tilePlacer.RemoveStartPositioningTilesListener(SetAppropriateVisibility);
+        tilePlacer.RemoveStopPositioningTilesListener(SetAppropriateVisibility);
+    }
+
+}
---
+++ Assets/Scripts/Tiles/TilePlacer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TilePlacer : MonoBehaviour {
+
+    public static TilePlacer instance;
+
+    private bool _IsPositioningTiles = false;
+    public bool IsPositioningTiles {
+        get { return _IsPositioningTiles; }
+        set {
+            _IsPositioningTiles = value;
+            if (value == true) {
+                foreach(OnStartPositioningTiles callback in StartPositioningCallbacks){
+                    callback();
+                }
+            }
+            if (value == false) {
+                foreach (OnStopPositioningTiles callback in StopPositioningCallbacks) {
+                    callback();
+                }
+            }
+        }
+    }
+
+    public delegate void OnStartPositioningTiles();
+    public delegate void OnStopPositioningTiles();
+
+    private List<OnStartPositioningTiles> StartPositioningCallbacks = new List<OnStartPositioningTiles>();
+    private List<OnStopPositioningTiles> StopPositioningCallbacks = new List<OnStopPositioningTiles>();
+
+    void Awake() {
+        instance = this;
+    }
+
+    public void TogglePositioningMode() {
+        IsPositioningTiles = !IsPositioningTiles;
+    }
+
+    public void AddStartPositioningTilesListener(OnStartPositioningTiles callback) {
+        StartPositioningCallbacks.Add(callback);
+    }
+
+    public void RemoveStartPositioningTilesListener(OnStartPositioningTiles callback) {
+        StartPositioningCallbacks.Remove(callback);
+    }
+
+    public void AddStopPositioningTilesListener(OnStopPositioningTiles callback) {
+        StopPositioningCallbacks.Add(callback);
+    }
+
+    public void RemoveStopPositioningTilesListener(OnStopPositioningTiles callback) {
+        StopPositioningCallbacks.Remove(callback);
+    }
+	
+}
