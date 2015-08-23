ce4d480016d85c1c8baa1947153b66e1de1b0c99
Michael Dawkins <michaeldawfr@gmail.com>
2015-08-23 21:50:04 +0200

    feat(StartWaveLabel): create PulsingText to make pulsing label

---
+++ Assets/Scripts/UI/PulsingText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class PulsingText : MonoBehaviour {
+
+    public float Speed;
+
+    Text text;
+
+    void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	void Update () {
+        text.SetAlpha(Mathf.Abs(Mathf.Cos(Time.realtimeSinceStartup * Speed)));
+    }
+}
